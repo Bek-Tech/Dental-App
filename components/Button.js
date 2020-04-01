51abd449a75842dcc3594755d7893c3149ae2e41
bekwdev <bekwdev@gmail.com>
2020-04-01 15:12:34 -0400

    added call button in Details Screen

--- components/Button.js
+++ components/Button.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-export default function Button({ title, color }) {
+export default function Button({ title, color, action, style }) {
+    Button.defaultProps = {
+        color: 'blue',
+        title: "default"
+    }
+
     return (
         <TouchableOpacity style={{
+            ...style,
+            marginRight: 5,
+            flex: 1,
             justifyContent: "center",
             alignItems: "center",
             borderRadius: 30,
             backgroundColor: color,
             height: 45
-        }}><ButtonText>{title}</ButtonText></TouchableOpacity>
+        }}
+            onPress={() => action()}
+        ><ButtonText>{title}</ButtonText></TouchableOpacity>
     )
 }
 
 
-const styles = StyleSheet.create({
-    button: {
-
-    }
-
-})
 
 const ButtonText = styled.Text`
 font-weight : 600
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -3,9 +3,9 @@ import { Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 import styled from 'styled-components/native'
 import Button from '../components/Button'
+import { Ionicons } from '@expo/vector-icons'
 
-
-const DetailsScreen = ({ }) => {
+const DetailsScreen = ({ navigation }) => {
 
 
     DetailsScreen.navigationOptions = {
@@ -24,7 +24,17 @@ const DetailsScreen = ({ }) => {
             <Container>
                 <Name>Customer Name</Name>
                 <GrayText>+1 *** *** ** **</GrayText>
-                <Button title={"Button"} color={"#4294ff"} />
+                <ButtonDiv>
+                    <Button style={{
+                        shadowOpacity: 0.5,
+                        shadowRadius: 6.3,
+                        elevation: 10
+                    }} color={"#4294ff"} action={() => navigation.navigate('PatientList')} />
+                    <CallButton onPress={() => navigation.navigate('Details')}>
+                        <Ionicons name="md-call" size={30} color="#fff" />
+                    </CallButton>
+                </ButtonDiv>
+
             </Container>
         </LinearGradient>
 
@@ -35,7 +45,28 @@ export default DetailsScreen
 
 // styles ___________________________________
 
+const CallButton = styled.TouchableOpacity`
 
+align-items:center
+justify-content: center
+border-radius: 50px
+width: 45px
+height: 45px
+background: #4CBE2E
+shadow-color: #000
+${'' /* shadow-offset: {width: 0, height: 2} */}
+shadow-opacity: 0.5
+shadow-radius: 6.3px
+elevation: 10
+`
+
+const ButtonDiv = styled.View`
+padding: 5px
+flex-direction: row
+justify-content: center
+align-items: center
+
+`
 
 
 const Name = styled.Text`
@@ -56,4 +87,5 @@ border-radius : 25px
 
 const GrayText = styled.Text`
  color : #8b979f
+ margin-bottom: 5px
 `
